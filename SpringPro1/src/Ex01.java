7ec8bc074ccfe09d417ad04b6015d5afecf7bf20
puk0806 <puk0806@naver.com>
2020-01-07 17:03:51 +0900

    Spring start

---
+++ SpringPro1/src/Ex01.java
@@ -0,0 +1,32 @@
+
+public class Ex01 {
+	
+	public static void main(String[] args) {
+		// 스프링 기초
+		// 스프링 DI, AOP
+		// 스프링 MVC
+		// 스프링 JDBC or 스프링 트랜잭션 처리
+		// ORM 연동 지원 -JPA, [mybatis] , 하이버네이트 등등...
+		// 스프링 시큐리티(보안) = 인증 권한
+		// 스프링 테스트 지원 = JUnit 테스트 지원 기능
+		
+		// 메이븐 (Maven) - 프로젝트 생성 -> 배포 (빌드 도구)
+		
+		// p29
+		// 자바를 이용해서 어플리케이션을 개발할 때 메이픈(Maven)이나 그래들(Gradle)과 같은 빌드 도구를 사용
+		// 이런 빌드 도구들의 [주요 특징] 중 하나는 의존 모듈 관리에 있다.
+		
+		// 메이븐의 경우 "중앙 리파지터리( central repository )"라고 불리는 서버로 부터 필요한 jar파일을 다운받아 의존 모듈을 관리한다.
+		
+		// pom.xml 의존 관리 설정파일- 설정
+		
+		// 패키지 선언 : net.madvirus.spring4.chap01
+		// p39 BuildRunner 인터페이스
+		//	MavenBuildRunner 클래스
+		// 	Project 클래스
+		//	Main 클래스
+		//	스프링을 사용하지 않고 객체 생성/조립/사용하는 예제
+		
+	}
+
+}
